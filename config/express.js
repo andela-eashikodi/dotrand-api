d5aced349e68db69bfbbcdf1dc8aed185e3a0ac3
Onye Ashikodi <ashikodiemeka@gmail.com>
2015-12-17 11:48:28 +0100

    add 404 page

--- config/express.js
+++ config/express.js
@@ -1,16 +1,14 @@
 'use strict';
 
 var express = require('express'),
-  morgan = require('morgan'),
   bodyParser = require('body-parser'),
   session = require('express-session'),
-  config = require('./config');
+  config = require('./config'),
+  path = require('path'),
+  appDir = path.dirname(require.main.filename);
 
 module.exports = function() {
   var app = express();
-  if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-  }
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(session({
@@ -23,6 +21,10 @@ module.exports = function() {
     res.header('Access-Control-Allow-Headers', "Origin, Accept, Content-Type, Access-Control-Allow-Headers, x_access_admin, Authorization, X-Requested-With");
     res.header('Access-Control-Allow-Methods', "POST, PUT, DELETE, GET");
     next();
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(appDir + '/404.html');
   });
 
   return app;
